a274580faaa472fe145e0aa15f6dedd2e7a22d23
Rodrigo Gomes <rodrigogr1602@gmail.com>
2023-12-27 13:28:17 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def configure(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://admin:CSgdtH2fRUggJ5FkOiBFYZEvxvIQGsed@dpg-cm64fdud3nmc73ccls50-a/todo_database_45hs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:CSgdtH2fRUggJ5FkOiBFYZEvxvIQGsed@dpg-cm64fdud3nmc73ccls50-a/todo_database_45hs'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
